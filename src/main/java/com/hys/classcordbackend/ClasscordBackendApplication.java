e1c2d28133c67fe739778299553a7d14f6a9101e
hys <186450715+hysyoi@users.noreply.github.com>
2026-06-10 08:06:23 +0800

    Initial commit

---
+++ src/main/java/com/hys/classcordbackend/ClasscordBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hys.classcordbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasscordBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClasscordBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hys/classcordbackend/ClasscordBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hys.classcordbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClasscordBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
